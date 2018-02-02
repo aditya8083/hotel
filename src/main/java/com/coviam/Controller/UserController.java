72309ac5be3d9d17b4086b9e64e39e3d725785de
aditya8083 <apandey8083@gmail.com>
2018-02-02 16:08:02 +0530

    config file change

--- src/main/java/com/coviam/Controller/UserController.java
+++ src/main/java/com/coviam/Controller/UserController.java
@@ -1,7 +1,16 @@
 package com.coviam.Controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(HttpServletRequest request) {
+        return "hello Aditya";
+    }
+
 }
